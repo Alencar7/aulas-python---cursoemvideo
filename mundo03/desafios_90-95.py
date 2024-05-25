44c60fdb04872a5fa01128288d5f7360e31de6e0
Adriano de Alencar <109616027+Alencar7@users.noreply.github.com>
2024-05-25 17:34:16 -0300

    Update desafios_90-95.py

--- mundo03/desafios_90-95.py
+++ mundo03/desafios_90-95.py
@@ -3,23 +3,80 @@
 Faça um prog que leia o nome e a media do aluno,
 garantindo tambem a situacao em um dicionario.
 No final, mostre o conteudo da estrutura na tela.
-'''
+
 #ex
-'''
 nome = adr
 media de adr = 9
 nome e igual a 'adr'
 media e igual a 9
 situacao e igual a aprovado
 '''
+#Resolucao 90
+'''
+
+lista = []
+dados = {}
+media = 7
+while True:
+    cond = str(input('Deseja entrar?\n[S/N]> '))
+    condicao = cond.upper()
+    if condicao =='S':
+        dados['nome'] = str(input('Nome: '))
+        dados['media'] = float(input('Media: '))
+        lista.append(dados.copy())
+    elif condicao == 'N':
+        print('Programa encerrado.')
+        break
+    else:
+        print('Tente novamente!')
+
+print('-='*34)
+print(lista)
+print('-='*34)
+for pessoa in lista:
+    print(f'Nome = {pessoa['nome']} ')
+    print(f'Media de {pessoa['nome']} = {pessoa['media']}')
+    if pessoa['media'] > media:
+        print(f'{pessoa['nome']} esta aprovado!')
+    else:
+        print(f'{pessoa['nome']} esta reprovado!')
+    print('-=' * 34)
+'''
+
 
 #Desafio 91
 '''
 Crie um programa onde 4 jogadores joguem um dado e enham resultados 
 aleatorios. Guarde-os em um dicionario. No final, coloque esse dicionario
 em ordem, sabendo que o vencedor tirou o maior numero no dado.
+# from operator import itemgetter
 '''
-#colocar o dicionario em ordem
+#Resolucao 91 ----------- #coisas de fora da aula #colocar o dicionario em ordem
+'''
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+ranking = ()
+jogo = {'jogador_1': randint(1,6),
+        'jogador_2': randint(1,6),
+        'jogador_3': randint(1,6),
+        'jogador_4': randint(1,6)}
+
+for n, v in jogo.items():
+    print(f'{n} tirou {v} no dado.')
+    sleep(1)
+
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+#key=itemgetter(1)=> (1) => para pegar apenas os valores int
+#print(ranking)
+print('-='*30)
+for posicao, v in enumerate(ranking):
+    #enumerate e para listas e o items() ara dicionarios
+    print(f'{posicao+1} lugar: {v[0]} com {v[1]}')
+    sleep(1.5)
+'''
+
 
 #Desafio 92
 '''
@@ -55,7 +112,6 @@ Aprimore o desafio #93 para que ele funcione com varios jogadores, incluindo
 um sistema de visualizacao de detalhes do aproveitamento de cada jogador.
 
 '''
-
 
 
 
